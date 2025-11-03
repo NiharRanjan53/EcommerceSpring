8c7ea0da19e2ca8a01ec9416e17ef20e4e0903c9
Nihar Ranjan <niharranjanrout53@gmail.com>
2025-11-03 22:59:15 +0530

    Added unit test case for Category service.

---
+++ src/test/java/com/nihar/ecommerce/services/CategoryServiceTest.java
@@ -0,0 +1,72 @@
+package com.nihar.ecommerce.services;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import com.nihar.ecommerce.dto.CategoryDTO;
+import com.nihar.ecommerce.entity.Category;
+import com.nihar.ecommerce.repository.CategoryRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+public class CategoryServiceTest {
+    @Mock
+    private CategoryRepository categoryRepository;
+    @InjectMocks
+    private CategoryService categoryService;
+
+    @Test
+    @DisplayName("Should return all categories successfully")
+    void getAllCategories_shouldReturnAllCategories() throws IOException {
+        // Arrange
+        List<Category> categories = new ArrayList<>();
+        Category category1 = Category.builder().name("Electronics").build();
+        category1.setId(1L);
+        Category category2 = Category.builder().name("Books").build();
+        category2.setId(2L);
+
+        categories.add(category1);
+        categories.add(category2);
+
+        when(categoryRepository.findAll()).thenReturn(categories);
+
+        // Act
+        List<CategoryDTO> result = categoryService.getAllCategories();
+
+        // Assert
+        assertEquals(result.size(), 2);
+
+    }
+
+    @Test
+    @DisplayName("Should return category by name")
+    void getByName_shouldReturnCategoryByName() throws Exception {
+        Category category1 = Category.builder().name("Electronics").build();
+        category1.setId(1L);
+
+        // Arrange
+        when(categoryRepository.findByName("Electronics")).thenReturn(Optional.of(category1));
+
+        // Act
+        CategoryDTO result = categoryService.getByName("Electronics");
+
+        // Assert
+        assertEquals(result.getName(), "Electronics");
+        verify(categoryRepository, times(1)).findByName("Electronics");
+
+    }
+}
